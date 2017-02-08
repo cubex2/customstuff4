f68b4d3fff54090cb62b17dd638686edc5173dd1
cubex2 <breuer.t.42@gmail.com>
2017-02-08 14:52:04 +0100

    Setup workspace

---
+++ src/main/java/cubex2/cs4/CustomStuff4.java
@@ -0,0 +1,11 @@
+package cubex2.cs4;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = CustomStuff4.ID, name = CustomStuff4.NAME, version = CustomStuff4.VERSION)
+public class CustomStuff4
+{
+    public static final String ID = "customstuff4";
+    public static final String NAME = "Custom Stuff 4";
+    public static final String VERSION = "0.0.1";
+}
