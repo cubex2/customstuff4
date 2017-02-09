ca71e776e6b373feb2e5bc3f13eb136e82e5eb83
cubex2 <breuer.t.42@gmail.com>
2017-02-09 19:29:42 +0100

    Fixed javadoc for Content

--- src/main/java/cubex2/cs4/api/Content.java
+++ src/main/java/cubex2/cs4/api/Content.java
@@ -3,7 +3,8 @@ package cubex2.cs4.api;
 public interface Content
 {
     /**
-     * Initialize the content. This is being called on all phases.
+     * Initialize the content. This is called on all phases if no 'initPhase' attribute
+     * is defined in the loader json.
      */
     void init(InitPhase phase, ContentHelper helper);
 }
