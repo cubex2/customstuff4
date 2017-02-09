44d219556c25978ce10f2d482c900d8c1bd6549a
cubex2 <breuer.t.42@gmail.com>
2017-02-09 15:20:03 +0100

    Added API annotation to api package

--- src/main/java/cubex2/cs4/api/package-info.java
+++ src/main/java/cubex2/cs4/api/package-info.java
@@ -1,7 +1,10 @@
 @MethodsReturnNonnullByDefault
 @ParametersAreNonnullByDefault
+@API(owner = CustomStuff4.ID, provides = "CustomStuff4API", apiVersion = "1")
 package cubex2.cs4.api;
 
+import cubex2.cs4.CustomStuff4;
 import mcp.MethodsReturnNonnullByDefault;
+import net.minecraftforge.fml.common.API;
 
 import javax.annotation.ParametersAreNonnullByDefault;
