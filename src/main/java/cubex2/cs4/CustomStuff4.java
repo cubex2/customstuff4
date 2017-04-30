d9791519931603afbcb996c7bd926bf788f347ff
cubex2 <breuer.t.42@gmail.com>
2017-04-30 15:51:13 +0200

    Bump version

--- src/main/java/cubex2/cs4/CustomStuff4.java
+++ src/main/java/cubex2/cs4/CustomStuff4.java
@@ -18,7 +18,7 @@ public class CustomStuff4
 {
     public static final String ID = "customstuff4";
     public static final String NAME = "Custom Stuff 4";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "0.1.0";
 
     public static final ContentRegistryImpl contentRegistry = new ContentRegistryImpl();
 
