73af72701b9183c136289276adbd30616df40349
cubex2 <breuer.t.42@gmail.com>
2017-05-05 14:18:21 +0200

    Added dropOnClose attribute to slots of container guis

--- src/main/java/cubex2/cs4/plugins/vanilla/gui/ContainerGui.java
+++ src/main/java/cubex2/cs4/plugins/vanilla/gui/ContainerGui.java
@@ -1,5 +1,7 @@
 package cubex2.cs4.plugins.vanilla.gui;
 
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import cubex2.cs4.plugins.vanilla.ContentGuiContainer;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.inventory.Container;
@@ -9,6 +11,9 @@ import net.minecraftforge.items.IItemHandler;
 import net.minecraftforge.items.SlotItemHandler;
 import net.minecraftforge.items.wrapper.PlayerMainInvWrapper;
 
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 public class ContainerGui extends Container
@@ -16,6 +21,7 @@ public class ContainerGui extends Container
     private final ContentGuiContainer content;
     private final ItemHandlerSupplier supplier;
     private final IItemHandler playerInv;
+    private final Map<SlotData, List<SlotItemHandler>> slotMap = Maps.newHashMap();
 
     public ContainerGui(ContentGuiContainer content, ItemHandlerSupplier supplier, EntityPlayer player)
     {
@@ -37,11 +43,19 @@ public class ContainerGui extends Container
                         int index = data.firstSlot + row * data.columns + col;
                         int x = data.x + col * 18;
                         int y = data.y + row * 18;
-                        addSlotToContainer(new SlotItemHandler(inv.get(), index, x, y));
+                        addSlot(data, new SlotItemHandler(inv.get(), index, x, y));
                     }
                 }
             }
         }
+    }
+
+    private void addSlot(SlotData data, SlotItemHandler slot)
+    {
+        addSlotToContainer(slot);
+        if (!slotMap.containsKey(data))
+            slotMap.put(data, Lists.newArrayList());
+        slotMap.get(data).add(slot);
     }
 
     private Optional<IItemHandler> getInventory(String name)
@@ -50,6 +64,32 @@ public class ContainerGui extends Container
             return Optional.of(playerInv);
         else
             return supplier.getItemHandler(name);
+    }
+
+    @Override
+    public void onContainerClosed(EntityPlayer playerIn)
+    {
+        super.onContainerClosed(playerIn);
+
+        if (!playerIn.world.isRemote)
+        {
+            for (SlotData data : content.slots)
+            {
+                if (data.dropOnClose)
+                {
+                    List<SlotItemHandler> slots = slotMap.getOrDefault(data, Collections.emptyList());
+                    for (SlotItemHandler slot : slots)
+                    {
+                        ItemStack stack = slot.decrStackSize(Integer.MAX_VALUE);
+
+                        if (!stack.isEmpty())
+                        {
+                            playerIn.dropItem(stack, false);
+                        }
+                    }
+                }
+            }
+        }
     }
 
     @Override
