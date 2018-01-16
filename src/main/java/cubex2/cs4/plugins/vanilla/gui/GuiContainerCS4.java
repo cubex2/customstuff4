cf4a83c68832f4690229113d58ce9aef094f3089
cubex2 <breuer.t.42@gmail.com>
2018-01-16 16:28:45 +0100

    Fixed item tooltips aren't shown in GUIs. Closes #47

--- src/main/java/cubex2/cs4/plugins/vanilla/gui/GuiContainerCS4.java
+++ src/main/java/cubex2/cs4/plugins/vanilla/gui/GuiContainerCS4.java
@@ -31,6 +31,14 @@ public class GuiContainerCS4 extends GuiContainer
     }
 
     @Override
+    public void drawScreen(int mouseX, int mouseY, float partialTicks)
+    {
+        drawDefaultBackground();
+        super.drawScreen(mouseX, mouseY, partialTicks);
+        renderHoveredToolTip(mouseX, mouseY);
+    }
+
+    @Override
     protected void drawGuiContainerBackgroundLayer(float partialTicks, int mouseX, int mouseY)
     {
         if (content.bg != null)
